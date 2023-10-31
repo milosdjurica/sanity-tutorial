4d3572901790b73046e4ffa8bee4a56e71971c4e
milosdjurica <milosdjurica13@gmail.com>
2023-10-31 18:39:31 +0100

    added portable text for content and displayed image on dynamic pages

--- src/app/projects/[project]/page.tsx
+++ src/app/projects/[project]/page.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { revalidatePath } from "next/cache";
 import { getProject } from "../../../../sanity/sanity-utils";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
 
 type Props = {
   params: { project: string };
@@ -8,9 +11,42 @@ type Props = {
 export default async function Project({ params }: Props) {
   const slug = params.project;
   const project = await getProject(slug);
+
+  // ! find better solution
+  revalidatePath(`/projects/${project.slug}`);
+
   return (
-    <div>
-      <p>{project.name}</p>
+    <div className="max-w-3xl mx-auto py-20">
+      <header className="flex justify-between items-center">
+        <h1
+          className="bg-gradient-to-r from-orange-400 to-purple-500 bg-clip-text 
+        text-transparent text-5xl drop-shadow font-extrabold"
+        >
+          {project.name}
+        </h1>
+        <a
+          className="bg-gray-100 rounded-lg text-gray-500 
+          font-bold py-3 px-4 whitespace-nowrap
+          hover:bg-pink-500 hover:text-pink-100 transition "
+          href={project.url}
+          title="View Project"
+          target="_blank"
+          rel="noopener noreferer"
+        >
+          View Project
+        </a>
+      </header>
+
+      <div className="text-lg text-gray-700 mt-5">
+        <PortableText value={project.content} />
+      </div>
+      <Image
+        src={project.image}
+        alt={project.name}
+        width={1920}
+        height={1080}
+        className="mt-10 border-2 border-gray-700 object-cover rounded-xl"
+      />
     </div>
   );
 }
