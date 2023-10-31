770472256f5a796b43cd2b7b17a048d2d13b5443
milosdjurica <milosdjurica13@gmail.com>
2023-10-31 17:31:31 +0100

    added image, dynamic routing to projects, minor stylings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { revalidatePath } from "next/cache";
 import { getProjects } from "../../sanity/sanity-utils";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   revalidatePath("/");
@@ -18,22 +19,28 @@ export default async function Home() {
       <p className="mt-3 text-xl text-gray-600">Hi everyone!</p>
       <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
 
-      {projects.map((project) => (
-        <div className="border border-gray-500 rounded-lg " key={project._id}>
-          {project.image && (
-            <Image
-              src={project.image}
-              alt={project.name}
-              width={250}
-              height={250}
-              className="object-cover rounded-lg border border-gray-500"
-            />
-          )}
-          <div className="font-extrabold bg-gradient-to-r from-orange-400 to-purple-500 bg-clip-text text-transparent">
-            {project.name}
-          </div>
-        </div>
-      ))}
+      <div className="mt-5 grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-8">
+        {projects.map((project) => (
+          <Link
+            href={`/projects/${project.slug}`}
+            className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition"
+            key={project._id}
+          >
+            {project.image && (
+              <Image
+                src={project.image}
+                alt={project.name}
+                width={750}
+                height={300}
+                className="object-cover rounded-lg border border-gray-500"
+              />
+            )}
+            <div className="mt-2 font-extrabold bg-gradient-to-r from-orange-400 to-purple-500 bg-clip-text text-transparent">
+              {project.name}
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
